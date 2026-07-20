8be745ff1075dacc3883dedcf8f318150887d5eb
Ehtisham Hassan <ehtishamh99@gmail.com>
2026-07-20 19:08:32 +0500

    Time: 3 ms (99%), Space: 20.8 MB (19.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,19 +2,11 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
 
         s = s.lower()
-        my_string = [ ch for ch in s if ch.isalnum()] # .alpha  includes alphabet chars and .alnum includes onlu nums and alphas
+        my_string = [ ch for ch in s if ch.isalnum()] 
+        # .alpha  includes alphabet chars and .alnum includes onlu nums and alphas
 
         return my_string == my_string[::-1]
-        # left = 0 
-        # right = len(my_string) - 1
 
-        # while left <= right:
-        #     if my_string[left] == my_string[right]:
-        #         left += 1
-        #         right -= 1
-        #     else:
-        #         return False
-        # return True
 
 
 
